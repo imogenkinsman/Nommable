9e9d9604fa2e666645c17679054fd89f9da1516a
Jade McGough <jade@thezets.com>
2013-11-25 01:16:06 -0800

    fixing bug where duplicates of the map view would crash the app

--- src/com/codepath/apps/nommable/fragments/MapFragment.java
+++ src/com/codepath/apps/nommable/fragments/MapFragment.java
@@ -1,19 +1,32 @@
 package com.codepath.apps.nommable.fragments;
 
-import com.codepath.apps.nommable.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.codepath.apps.nommable.R;
+
 public class MapFragment extends Fragment {
+	
+	private static View view;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_map, container, false);
+		if (view != null) {
+			ViewGroup parent = (ViewGroup) view.getParent();
+			if (parent != null)
+				parent.removeView(view);
+		}
+		try {
+			view = inflater.inflate(R.layout.fragment_map, container, false);
+		} catch (InflateException e) {
+			/* map is already there, just return view as it is */
+		}
+		return view;
 	}
 	
 }
