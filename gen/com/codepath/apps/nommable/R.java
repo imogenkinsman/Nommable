0cedb99d004a256126eac7c9e16ad085eb563d39
Jade McGough <jade@thezets.com>
2013-11-26 21:04:50 -0800

    removing yelp login activity

--- gen/com/codepath/apps/nommable/R.java
+++ gen/com/codepath/apps/nommable/R.java
@@ -272,16 +272,15 @@ containing a value of this type.
     public static final class layout {
         public static final int activity_history=0x7f030000;
         public static final int activity_launch=0x7f030001;
-        public static final int activity_login=0x7f030002;
-        public static final int activity_map=0x7f030003;
-        public static final int activity_results=0x7f030004;
-        public static final int activity_search=0x7f030005;
-        public static final int dialog_rating=0x7f030006;
-        public static final int fragment_details=0x7f030007;
-        public static final int fragment_map=0x7f030008;
-        public static final int fragment_menu=0x7f030009;
-        public static final int item_restaurant=0x7f03000a;
-        public static final int windowlayout=0x7f03000b;
+        public static final int activity_map=0x7f030002;
+        public static final int activity_results=0x7f030003;
+        public static final int activity_search=0x7f030004;
+        public static final int dialog_rating=0x7f030005;
+        public static final int fragment_details=0x7f030006;
+        public static final int fragment_map=0x7f030007;
+        public static final int fragment_menu=0x7f030008;
+        public static final int item_restaurant=0x7f030009;
+        public static final int windowlayout=0x7f03000a;
     }
     public static final class menu {
         public static final int history=0x7f0b0000;
