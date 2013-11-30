919540295bb0d559cbbce689647c3405d3b14491
Jade McGough <jade@thezets.com>
2013-11-30 05:07:57 -0800

    adding actionbar sherlock

--- src/com/codepath/apps/nommable/activities/ResultsActivity.java
+++ src/com/codepath/apps/nommable/activities/ResultsActivity.java
@@ -3,18 +3,17 @@ package com.codepath.apps.nommable.activities;
 import java.util.ArrayList;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.Menu;
+import com.actionbarsherlock.view.Menu;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.codepath.apps.nommable.R;
 import com.codepath.apps.nommable.adapters.ResultsPagerAdapter;
 import com.codepath.apps.nommable.fragments.DetailsFragment;
 import com.codepath.apps.nommable.fragments.MapFragment.OnRestaurantChangedListener;
 import com.codepath.apps.nommable.models.Restaurant;
 
-public class ResultsActivity extends FragmentActivity implements OnRestaurantChangedListener {
+public class ResultsActivity extends SherlockFragmentActivity implements OnRestaurantChangedListener {
 	ArrayList<Restaurant> restaurants;	
 	ResultsPagerAdapter rpAdapter;
 
@@ -22,14 +21,20 @@ public class ResultsActivity extends FragmentActivity implements OnRestaurantCha
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_results);
-		getActionBar().hide();
 
 		ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
 		restaurants = (ArrayList<Restaurant>) getIntent().getSerializableExtra("restaurants");
 		rpAdapter = new ResultsPagerAdapter(getSupportFragmentManager(), restaurants);
 		pager.setAdapter(rpAdapter);
 	}
-
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getSupportMenuInflater().inflate(R.menu.search, menu);
+		return true;
+	}
+	
 	@Override
 	public void onRestaurantChanged(Restaurant rest) {
 		DetailsFragment details = (DetailsFragment) rpAdapter.getFragment(1);
