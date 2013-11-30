c71c989958b78eb2efc659154d43510744fb1520
Jade McGough <jade@thezets.com>
2013-11-30 05:38:11 -0800

    send current restaurant info when changing pages instead of in response to a click

--- src/com/codepath/apps/nommable/activities/ResultsActivity.java
+++ src/com/codepath/apps/nommable/activities/ResultsActivity.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import com.actionbarsherlock.view.Menu;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
 import com.codepath.apps.nommable.R;
 import com.codepath.apps.nommable.adapters.ResultsPagerAdapter;
 import com.codepath.apps.nommable.fragments.DetailsFragment;
@@ -16,6 +17,7 @@ import com.codepath.apps.nommable.models.Restaurant;
 public class ResultsActivity extends SherlockFragmentActivity implements OnRestaurantChangedListener {
 	ArrayList<Restaurant> restaurants;	
 	ResultsPagerAdapter rpAdapter;
+	Restaurant currentRestaurant;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +26,39 @@ public class ResultsActivity extends SherlockFragmentActivity implements OnResta
 
 		ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
 		restaurants = (ArrayList<Restaurant>) getIntent().getSerializableExtra("restaurants");
+		currentRestaurant = restaurants.get(0);
 		rpAdapter = new ResultsPagerAdapter(getSupportFragmentManager(), restaurants);
 		pager.setAdapter(rpAdapter);
+		
+		pager.setOnPageChangeListener(new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int position) {
+				if (position == 1) {
+					DetailsFragment details = (DetailsFragment) rpAdapter.getFragment(1);
+					details.updateText(currentRestaurant);
+				}
+			}
+			
+			@Override
+			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int state) {
+			}
+		});;
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getSupportMenuInflater().inflate(R.menu.search, menu);
 		return true;
 	}
 	
 	@Override
 	public void onRestaurantChanged(Restaurant rest) {
-		DetailsFragment details = (DetailsFragment) rpAdapter.getFragment(1);
-		details.updateText(rest);
+		currentRestaurant = rest;
 	}
 	
 }
