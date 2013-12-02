10d49bd08ae733d35d5c9c7ae60d5dec1f3bead9
Jade McGough <jade@thezets.com>
2013-12-02 14:33:37 -0800

    changing menuadapter to inflate more than one view type

--- src/com/codepath/apps/nommable/adapters/MenuAdapter.java
+++ src/com/codepath/apps/nommable/adapters/MenuAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.codepath.apps.nommable.R;
 import com.codepath.apps.nommable.models.MenuEntry;
+import com.codepath.apps.nommable.models.MenuEntry.Type;
 
 public class MenuAdapter extends ArrayAdapter<MenuEntry> {
 
@@ -18,18 +19,59 @@ public class MenuAdapter extends ArrayAdapter<MenuEntry> {
 		super(context, 0, entries);
 	}
 
+	@Override
+	public int getViewTypeCount() {
+		return 3;
+	}
+	
+	@Override
+	public int getItemViewType(int position) {
+		Type type = (Type) getItem(position).getType();
+				
+		switch (type) {
+		case MENU:
+			return 0;
+		case SECTION:
+			return 1;
+		case ENTRY:
+			return 2;
+		default:
+			return 0;
+		}
+	}
+	
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = convertView;
 		if (view == null) {
+			int type =  getItemViewType(position);
 			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = inflater.inflate(R.layout.item_entry, null);
-		}
-		
-		final MenuEntry entry = getItem(position);
-		
-		TextView tvName = (TextView) view.findViewById(R.id.tvName);
-		tvName.setText(entry.getName());
-		
+			
+			final MenuEntry entry = getItem(position);
+			TextView tvName;
+			
+			switch (type) {
+			case 0:
+				view = inflater.inflate(R.layout.item_entry, null);
+				tvName = (TextView) view.findViewById(R.id.tvName);
+				tvName.setText(entry.getName());
+				break;
+			case 1:
+				view = inflater.inflate(R.layout.item_category, null);
+				TextView tvCategory = (TextView) view.findViewById(R.id.tvCategory);
+				tvCategory.setText(entry.getName());
+				break;
+			case 2:
+				view = inflater.inflate(R.layout.item_entry, null);
+				tvName = (TextView) view.findViewById(R.id.tvName);
+				tvName.setText(entry.getName());
+				break;
+			default:
+				view = inflater.inflate(R.layout.item_entry, null);
+				tvName = (TextView) view.findViewById(R.id.tvName);
+				tvName.setText(entry.getName());
+				break;
+			}			
+		}		
 		return view;
 	}
 }
