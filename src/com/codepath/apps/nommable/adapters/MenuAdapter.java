cb638c299c7b56ec078fc3fbfa21a15aec3163f3
Jade McGough <jade@thezets.com>
2013-12-03 03:44:35 -0800

    fixing menuadapter duplication bug

--- src/com/codepath/apps/nommable/adapters/MenuAdapter.java
+++ src/com/codepath/apps/nommable/adapters/MenuAdapter.java
@@ -3,6 +3,7 @@ package com.codepath.apps.nommable.adapters;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,11 @@ import com.codepath.apps.nommable.models.MenuEntry.Type;
 
 public class MenuAdapter extends ArrayAdapter<MenuEntry> {
 
+	private LayoutInflater inflater;
+	
 	public MenuAdapter(Context context, List<MenuEntry> entries) {
 		super(context, 0, entries);
+		inflater = LayoutInflater.from(context);
 	}
 
 	@Override
@@ -40,42 +44,46 @@ public class MenuAdapter extends ArrayAdapter<MenuEntry> {
 		}
 	}
 	
+	//TODO: create a viewholder class to improve view creation speed
+	
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = convertView;
+		int type =  getItemViewType(position);
+
 		if (view == null) {
-			int type =  getItemViewType(position);
-			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			
-			final MenuEntry entry = getItem(position);
-			TextView tvName;
-			
 			switch (type) {
 			case 0:
 				view = inflater.inflate(R.layout.item_entry, null);
-				tvName = (TextView) view.findViewById(R.id.tvName);
-				tvName.setText(entry.getName());
 				break;
 			case 1:
 				view = inflater.inflate(R.layout.item_category, null);
-				TextView tvCategory = (TextView) view.findViewById(R.id.tvCategory);
-				tvCategory.setText(entry.getName());
 				break;
 			case 2:
 				view = inflater.inflate(R.layout.item_entry, null);
-				tvName = (TextView) view.findViewById(R.id.tvName);
+				break;
+			default:
+				view = inflater.inflate(R.layout.item_entry, null);
+				break;
+			}
+		}
+		MenuEntry entry = getItem(position);
+		if (entry != null) {
+			if (type == 0) {
+				TextView tvName = (TextView) view.findViewById(R.id.tvName);
+				tvName.setText(entry.getName());
+			} else if (type == 1) {
+				TextView tvCategory = (TextView) view.findViewById(R.id.tvCategory);
+				tvCategory.setText(entry.getName());
+			} else if (type == 2) {
+				TextView tvName = (TextView) view.findViewById(R.id.tvName);
 				tvName.setText(entry.getName());
 				TextView tvDescription = (TextView) view.findViewById(R.id.tvDescription);
 				tvDescription.setText(entry.getDescription());
 				TextView tvPrice = (TextView) view.findViewById(R.id.tvPrice);
 				tvPrice.setText(entry.getPrice());
-				break;
-			default:
-				view = inflater.inflate(R.layout.item_entry, null);
-				tvName = (TextView) view.findViewById(R.id.tvName);
-				tvName.setText(entry.getName());
-				break;
-			}			
-		}		
+			}
+
+		}
 		return view;
 	}
 }
--- src/com/codepath/apps/nommable/fragments/MenuFragment.java
+++ src/com/codepath/apps/nommable/fragments/MenuFragment.java
@@ -1,19 +1,17 @@
 package com.codepath.apps.nommable.fragments;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.codepath.apps.nommable.NommableApp;
 import com.codepath.apps.nommable.R;
